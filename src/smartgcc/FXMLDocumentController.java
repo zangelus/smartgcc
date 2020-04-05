dabd7e30e463a3c6709fa1a0b4ede3c02bec4b49
unknown <zangelus@gmail.com>
2020-04-04 22:35:14 -0400

    add return to select user btn

--- src/smartgcc/FXMLDocumentController.java
+++ src/smartgcc/FXMLDocumentController.java
@@ -6,18 +6,25 @@
 package smartgcc;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  *
@@ -99,6 +106,15 @@ public class FXMLDocumentController implements Initializable {
     private void handleBtnCancel(ActionEvent event) {
         listview2.getItems().add("Closing the app");
         Platform.exit();
+    }
+
+    @FXML
+    private void handleBtnSelectUser(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("UserSelection.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
     }
     
 }
